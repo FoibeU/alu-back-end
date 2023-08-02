3afec4af4818344ec4302a57bfe503b88d0e8624
root <f.uwizeyima@alueducation.com>
2023-08-02 03:27:23 -0700

    Task 0 gather info

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
 """
-Uses https://jsonplaceholder.typicode.com with an employee ID to
-return info on the employee's todo list progress
+A Script that, uses this REST API, for a given employee ID, returns
+information about his/her TODO list progress
 """
 
+import json
 import requests
-import sys
+from sys import argv
 
-"""Module"""
 
-if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
+if __name__ == "__main__":
 
-    user_info = requests.request('GET', user_url).json()
-    todos_info = requests.request('GET', todos_url).json()
+    sessionReq = requests.Session()
 
-    employee_name = user_info.get("name")
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    name = employeeName.json()['name']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
 
     print("Employee {} is done with tasks({}/{}):".
-          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+          format(name, totalTasks, len(json_req)))
 
-    [print("\t " + task["title"]) for task in task_completed]
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))
